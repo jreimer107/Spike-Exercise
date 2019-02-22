1b6f8d1c41038ff8026f9346e6ba92a7a322ec06
John Reimer <jreimer107@gmail.com>
2019-02-22 17:03:44 -0600

    selectors now operate off of firebase data

--- src/components/assignments/AssignmentSelect.jsx
+++ src/components/assignments/AssignmentSelect.jsx
@@ -3,97 +3,105 @@ import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { switchDisplayedAssignment } from '../../store/actions/assignmentActions';
-import { getFirestore } from 'redux-firestore';
 
 class AssignmentSelect extends Component {
-	handleClick = e => {
-		this.props.switchDisplayedAssignment(
-			this.props.assignments.find(x => x.id === e.target.id)
-		);
-	};
+    //When assignment dropdown option is clicked, it becomes the 'displayed assignment'
+    handleClick = e => {
+        this.props.switchDisplayedAssignment(
+            this.props.assignments.find(x => x.id === e.target.id),
+        );
+    };
 
-	render() {
-		const {
-			displayedAssignment,
-			displayedCourse,
-			assignments
-		} = this.props;
+    render() {
+        //Destructure props to simplify
+        const {
+            displayedAssignment,
+            displayedCourse,
+            assignments,
+        } = this.props;
 
-		const assignmentObj = displayedAssignment
-			? assignments.find(x => x.id === displayedAssignment.id)
-			: null;
+        //Get reference to displayed assignment object in firebase
+        const assignmentObj = displayedAssignment
+            ? assignments.find(x => x.id === displayedAssignment.id)
+            : null;
 
-		return (
-			<div className='container'>
-				<div className='dropdown'>
-					{/* Container for dropdown element. When not focused, shows selected assignment's name*/}
-					<button
-						className='btn btn-info dropdown-toggle m-2'
-						type='button'
-						id='dropdownMenu2'
-						data-toggle='dropdown'
-						aria-haspopup='true'
-						aria-expanded='false'
-					>
-						{assignmentObj.name || 'Select Assignment'}
-					</button>
-					<div
-						className='dropdown-menu'
-						aria-labelledby='dropdownMenu2'
-					>
-						{/* Dropdown elements. Displays elements that belong to selected course */}
-						{assignments &&
-							assignments
-								.filter(
-									x =>
-										x.course ===
-										this.props.displayedCourse.name
-								)
-								.map(assignment => {
-									return (
-										<button
-											className='dropdown-item'
-											type='button'
-											onClick={this.handleClick}
-											key={assignment.id}
-											id={assignment.id}
-										>
-											{assignment.name}
-										</button>
-									);
-								})}
-					</div>
-					{this.props.displayedAssignment ? (
-						<h5>
-							{assignmentObj.grade}
-							{assignmentObj.letterGrade}
-						</h5>
-					) : null}
-				</div>
-			</div>
-		);
-	}
+        return (
+            <div className='container'>
+                <div className='dropdown'>
+                    {/* Container for dropdown element. When not focused, shows selected assignment's name*/}
+                    <button
+                        className='btn btn-info dropdown-toggle m-2'
+                        type='button'
+                        id='dropdownMenu2'
+                        data-toggle='dropdown'
+                        aria-haspopup='true'
+                        aria-expanded='false'
+                    >
+                        {/* Display name of currently selected assignment. if none, display 'select assignment' */}
+                        {assignmentObj
+                            ? assignmentObj.name
+                            : 'Select Assignment'}
+                    </button>
+                    <div
+                        className='dropdown-menu'
+                        aria-labelledby='dropdownMenu2'
+                    >
+                        {/* Dropdown elements. Displays elements that belong to selected course */}
+                        {assignments &&
+                            displayedCourse &&
+                            assignments
+                                .filter(x => x.course === displayedCourse.id)
+                                .map(assignment => {
+                                    console.log('assignment:', assignment);
+                                    return (
+                                        <button
+                                            className='dropdown-item'
+                                            type='button'
+                                            onClick={this.handleClick}
+                                            key={assignment.id}
+                                            id={assignment.id}
+                                        >
+                                            {assignment.name}
+                                        </button>
+                                    );
+                                })}
+                    </div>
+                    {/* Display number and lettergrades of selected assignment */}
+                    {assignmentObj ? (
+                        <h5>
+                            {assignmentObj.grade.toFixed(2)}{' '}
+                            {assignmentObj.letterGrade}
+                        </h5>
+                    ) : null}
+                </div>
+            </div>
+        );
+    }
 }
 
+//Need to access ids of displayed course for reading
+//displayed assignment for reading and writing
+//assignments for reading
 const mapStateToProps = state => {
-	return {
-		displayedCourse: state.assignment.displayedCourse,
-		displayedAssignment: state.assignment.displayedAssignment,
-		assignments: state.firestore.ordered.assignments
-	};
+    return {
+        displayedCourse: state.assignment.displayedCourse,
+        displayedAssignment: state.assignment.displayedAssignment,
+        assignments: state.firestore.ordered.assignments,
+    };
 };
 
+//Dispatch action to switch displayed assignment
 const mapDispatchToProps = dispatch => {
-	return {
-		switchDisplayedAssignment: displayedAssignment =>
-			dispatch(switchDisplayedAssignment(displayedAssignment))
-	};
+    return {
+        switchDisplayedAssignment: displayedAssignment =>
+            dispatch(switchDisplayedAssignment(displayedAssignment)),
+    };
 };
 
 export default compose(
-	connect(
-		mapStateToProps,
-		mapDispatchToProps
-	),
-	firestoreConnect([{ collection: 'assignments' }])
+    connect(
+        mapStateToProps,
+        mapDispatchToProps,
+    ),
+    firestoreConnect([{ collection: 'assignments' }]),
 )(AssignmentSelect);
--- src/components/assignments/CourseSelect.jsx
+++ src/components/assignments/CourseSelect.jsx
@@ -6,74 +6,83 @@ import { switchDisplayedCourse } from '../../store/actions/assignmentActions';
 import { switchDisplayedAssignment } from '../../store/actions/assignmentActions';
 
 class CourseSelect extends Component {
-	handleClick = e => {
-		this.props.switchDisplayedAssignment('');
-		this.props.switchDisplayedCourse(
-			this.props.courses.find(x => x.id === e.target.id)
-		);
-	};
+    handleClick = e => {
+        this.props.switchDisplayedAssignment('');
+        this.props.switchDisplayedCourse(
+            this.props.courses.find(x => x.id === e.target.id),
+        );
+    };
 
-	render() {
-		console.log(this.props);
-		return (
-			<div className='container'>
-				<div className='dropdown'>
-					<button
-						className='btn btn-info dropdown-toggle m-2'
-						type='button'
-						id='dropdownMenu2'
-						data-toggle='dropdown'
-						aria-haspopup='true'
-						aria-expanded='false'
-					>
-						{this.props.displayedCourse.name || 'Select Course'}
-					</button>
-					<div
-						className='dropdown-menu'
-						aria-labelledby='dropdownMenu2'
-					>
-						{this.props.courses &&
-							this.props.courses.map(course => {
-								return (
-									<button
-										className='dropdown-item'
-										type='button'
-										onClick={this.handleClick}
-										key={course.id}
-										id={course.id}
-									>
-										{course.name}
-									</button>
-								);
-							})}
-					</div>
-				</div>
-			</div>
-		);
-	}
+    render() {
+        const { displayedCourse, courses } = this.props;
+        const courseObj = displayedCourse
+            ? courses.find(x => x.id === displayedCourse.id)
+            : null;
+        return (
+            <div className='container'>
+                <div className='dropdown'>
+                    {/* dropdown menu containing existing classes */}
+                    <button
+                        className='btn btn-info dropdown-toggle m-2'
+                        type='button'
+                        id='dropdownMenu2'
+                        data-toggle='dropdown'
+                        aria-haspopup='true'
+                        aria-expanded='false'
+                    >
+                        {courseObj ? courseObj.name : 'Select Course'}
+                    </button>
+                    <div
+                        className='dropdown-menu'
+                        aria-labelledby='dropdownMenu2'
+                    >
+                        {courses &&
+                            courses.map(course => {
+                                return (
+                                    <button
+                                        className='dropdown-item'
+                                        type='button'
+                                        onClick={this.handleClick}
+                                        key={course.id}
+                                        id={course.id}
+                                    >
+                                        {course.name}
+                                    </button>
+                                );
+                            })}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
+//props
+//need to read and write displayed course
+//need to write (reset) displayed course
+//need to read firebase collection of courses to display
 const mapStateToProps = state => {
-	return {
-		displayedCourse: state.assignment.displayedCourse,
-		displayedAssignment: state.assignment.displayedAssignment,
-		courses: state.firestore.ordered.courses
-	};
+    return {
+        displayedCourse: state.assignment.displayedCourse,
+        displayedAssignment: state.assignment.displayedAssignment,
+        courses: state.firestore.ordered.courses,
+    };
 };
 
+//Dispatch actions to switch displayed course and assignment
 const mapDispatchToProps = dispatch => {
-	return {
-		switchDisplayedCourse: displayedCourse =>
-			dispatch(switchDisplayedCourse(displayedCourse)),
-		switchDisplayedAssignment: displayedAssignment =>
-			dispatch(switchDisplayedAssignment(displayedAssignment))
-	};
+    return {
+        switchDisplayedCourse: displayedCourse =>
+            dispatch(switchDisplayedCourse(displayedCourse)),
+        switchDisplayedAssignment: displayedAssignment =>
+            dispatch(switchDisplayedAssignment(displayedAssignment)),
+    };
 };
 
 export default compose(
-	connect(
-		mapStateToProps,
-		mapDispatchToProps
-	),
-	firestoreConnect([{ collection: 'courses' }])
+    connect(
+        mapStateToProps,
+        mapDispatchToProps,
+    ),
+    firestoreConnect([{ collection: 'courses' }]),
 )(CourseSelect);
